d6a1a2e21306d65678bb9d71d1f66246bdc96af6
Somya Gupta <2018ucp1691@mnit.ac.in>
2020-06-18 16:28:32 +0530

    chnaged auth

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ passport.deserializeUser(function(id, done){
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/blog",
+    callbackURL: "https://obscure-caverns-86886.herokuapp.com/auth/google/blog",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
